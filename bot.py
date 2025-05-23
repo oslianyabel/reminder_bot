9c32cbf94ddfed303def26807ddec82d73dc1917
oslianyabel <oslianyabel@gmail.com>
2025-05-23 10:54:31 -0400

    self in bot

--- bot.py
+++ bot.py
@@ -7,13 +7,14 @@ load_dotenv()
 
 
 class MyBot(telebot.TeleBot):
-    def __init__(token, **kwargs):
+    def __init__(self, token, **kwargs):
         super().__init__(token, **kwargs)
 
     def send_message(self, chat_id, text, **kwargs):
-        bot.send_chat_action(chat_id, "typing")
+        self.send_chat_action(chat_id, "typing")
         return super().send_message(chat_id, text, **kwargs)
 
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")
+
 bot = MyBot(TOKEN)
